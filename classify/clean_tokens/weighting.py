565d5ea8cc1511fdbea0b1b2000afc7527638118
Kevin Cai <kevincai3@users.noreply.github.com>
2018-06-23 22:42:32 -0400

    Added weights for word occurences

--- classify/clean_tokens/weighting.py
+++ classify/clean_tokens/weighting.py
@@ -2,7 +2,7 @@ import csv
 
 weighting = {}
 
-with open('weights.csv') as datafile:
+with open('./clean_tokens/weights.csv') as datafile:
     reader = csv.reader(datafile)
     for row in reader:
         weighting[row[0]] = int(row[1])
