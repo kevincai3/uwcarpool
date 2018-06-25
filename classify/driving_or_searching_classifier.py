2bdd8afa093fbe3166f41923df7030f82e4ba826
AshwinPuri13 <37940966+AshwinPuri13@users.noreply.github.com>
2018-06-25 01:33:44 -0400

    Add files via upload

---
+++ classify/driving_or_searching_classifier.py
@@ -0,0 +1,28 @@
+from utils.carpool_data import posts
+from clean_tokens.process import process, stage3
+
+import pandas as pd
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics import precision_recall_fscore_support as score
+from sklearn.model_selection import train_test_split
+
+def analyzer(text):
+	stage3(process(text))
+
+count_vect = CountVectorizer(analyzer = analyzer)
+
+#the training set of posts for number of routes
+#in particular, the classified posts for number of routes
+posts_tr_dos = posts[~posts["driving_or_searching"].isnull()].reset_index(drop = True)
+
+X_count = count_vect.fit_transform(posts_tr_dos["message"])
+X_count_feat = pd.DataFrame(X_count.toarray())
+Y = posts_tr_dos["driving_or_searching"]
+
+
+def dos_classifier(message):
+    rf = RandomForestClassifier(n_estimators = 60, max_depth = 10, n_jobs=-1)
+    rf_model = rf.fit(X_count_feat,Y)
+    X = count_vect.fit(posts_tr_dos["message"])
+    message_turn_vector = X.transform([message])
+    return rf_model.predict(message_turn_vector)
