ccc58868a24df8df58380ea4a9181e22d0266661
Ashwin <a24puri@edu.uwaterloo.ca>
2018-09-11 19:06:22 -0400

    no longer time dependant

--- classify/update_tables/obtain_new_posts.py
+++ classify/update_tables/obtain_new_posts.py
@@ -1,10 +1,10 @@
 import pandas as pd
-import datetime
 from db import engine
 
-def obtain_new_posts(hourdelta = 1):
-    posts = pd.read_sql_query('select id, message, posttime from posts', con=engine)
-    posts["posttime"] = pd.to_datetime(posts["posttime"])
-    current_datetime = datetime.datetime.now()
-    last_hour_datetime = current_datetime - datetime.timedelta(hours = hourdelta)
-    return posts[posts["posttime"] > last_hour_datetime]
+def obtain_new_posts():
+    all_posts = pd.read_sql_query('select post_id, message, posttime from posts', con=engine)
+    all_posts_id = pd.DataFrame({"post_id":all_posts["post_id"]})
+    old_derived_posts_id = pd.read_sql_query('select post_id from derived_posts', con=engine)
+    new_posts_id = pd.concat([all_posts_id, old_derived_posts_id]).drop_duplicates(keep = False)
+    new_posts = all_posts.loc[all_posts["post_id"].isin(new_posts_id["post_id"])]
+    return new_posts
--- classify/update_tables/update_derived_posts.py
+++ classify/update_tables/update_derived_posts.py
@@ -1,6 +1,6 @@
 import pandas as pd
 from db import engine
-from clean_tokens.process import process
+from clean_tokens.process import process 
 
 def update_derived_posts(new_posts):
     new_posts_stage_1 = new_posts.apply(lambda x: process(x["message"], False, False), axis = 1)
@@ -10,6 +10,7 @@ def update_derived_posts(new_posts):
 	"post_id":new_posts.index,
 	"stage_1": new_posts_stage_1,
 	"stage_2": new_posts_stage_2,
-	"stage_3": new_posts_stage_3})
-    new_derived_posts.to_sql("derived_posts", engine, if_exists='append', chunksize= 10000, index = False)
+	"stage_3": new_posts_stage_3,
+	"posttime": new_posts["posttime"]})
+    new_derived_posts.drop(["posttime"], axis = 1).to_sql("derived_posts", engine, if_exists='append', chunksize= 10000, index = False)
     return new_derived_posts
