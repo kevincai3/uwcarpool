1481f9d39fee2cb3143f29dd0f502746e667e520
Ashwin <a24puri@edu.uwaterloo.ca>
2018-09-11 16:36:28 -0400

    initial commit

---
+++ classify/update_tables/update_derived_posts.py
@@ -0,0 +1,14 @@
+import pandas as pd
+from db import engine
+
+def update_derived_posts(new_posts):
+    new_posts_stage_1 = new_posts.apply(lambda x: process(x["message"], False, False), axis = 1)
+    new_posts_stage_2 = new_posts.apply(lambda x: process(x["message"], True, False), axis = 1)
+    new_posts_stage_3 = new_posts.apply(lambda x: process(x["message"]), axis = 1)
+    new_derived_posts = pd.DataFrame({
+	"post_id":new_posts.index,
+	"stage_1": new_posts_stage_1,
+	"stage_2": new_posts_stage_2,
+	"stage_3": new_posts_stage_3})
+    new_derived_posts.to_sql("derived_posts", engine, if_exists='append', chunksize= 10000, index = False)
+    return new_derived_posts
