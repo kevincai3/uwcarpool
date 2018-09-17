b3966390cea952e71bd5091546f5a1076a126c8a
Kevin Cai <kevincai3@users.noreply.github.com>
2018-09-16 20:00:30 -0400

    Added flag for update_tables

--- classify/update_tables/update_tables.py
+++ classify/update_tables/update_tables.py
@@ -6,8 +6,13 @@ from update_tables.update_estimate_posts import update_estimate_posts
 from update_tables.update_groups import update_groups
 from update_tables.update_trips import update_trips
 
-def update_tables():
-    new_posts = obtain_new_posts()
+def update_tables(all_posts = False):
+    new_posts = None
+    if all_posts:
+        new_posts = obtain_posts()
+    else:
+        new_posts = obtain_new_posts()
+
     new_derived_posts = update_derived_posts(new_posts)
     new_estimate_posts = update_estimate_posts(new_derived_posts)
     new_posts, new_groups = update_groups(new_posts)
