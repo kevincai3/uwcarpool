08ee90b7bdfb6afff959c7ce505af119673ca1dd
Kevin Cai <kevincai3@users.noreply.github.com>
2018-09-28 13:51:26 -0400

    Allow script to terminate

--- website/scripts/updateDB.js
+++ website/scripts/updateDB.js
@@ -1,19 +1,25 @@
 import { runUpdates } from '../src/pythonClient.js';
 import shell from 'shelljs';
 import fs from 'fs';
+import db from '../src/data/db/db.js';
 
 function runCommand() {
   try {
-    const path = '../../../bin/scrap-fb';
+    const path = '../../bin/scrap-fb';
     fs.accessSync(path, fs.constants.X_OK);
     shell.exec(path, (code, stdout, stderr) => {
       console.log('Exit code:', code);
       console.log('Program output:', stdout);
       console.log('Program stderr:', stderr);
-      runUpdates().then(rows => console.log(rows));
+      runUpdates()
+        .then(rows => console.log(rows))
+        .then(() => {
+          db.destroy();
+        });
     });
   } catch (error) {
     console.log(error);
+    db.destroy();
   }
 }
 
