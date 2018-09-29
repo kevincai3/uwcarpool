01908eede5e342ec951c0b1348264fac73785ca9
Kevin Cai <kevincai3@users.noreply.github.com>
2018-09-29 16:19:57 -0400

    Catch bug where no new trips are found causing crash

--- classify/update_tables/update_trips.py
+++ classify/update_tables/update_trips.py
@@ -29,10 +29,11 @@ def update_trips(new_estimate_posts, new_groups):
     new_trips = new_trips[new_trips["post_type"] != "o"]
     new_trips = splitDataFrameList(splitDataFrameList(new_trips, "from_loc", ","), "to_loc", ",")
 
-    values = [(group_id, post_type, pandas_nat_to_none(best_date), pandas_nat_to_none(best_time), from_loc, to_loc)
-              for group_id, post_type, best_date, best_time, from_loc, to_loc
-              in new_trips[['group_id', 'post_type', 'date', 'time', 'from_loc', 'to_loc']].values]
+    if len(new_trips) != 0:
+        values = [(group_id, post_type, pandas_nat_to_none(best_date), pandas_nat_to_none(best_time), from_loc, to_loc)
+                  for group_id, post_type, best_date, best_time, from_loc, to_loc
+                  in new_trips[['group_id', 'post_type', 'date', 'time', 'from_loc', 'to_loc']].values]
 
-    batch_update(INSERT_STATEMENT, values, 1000, 10000)
+        batch_update(INSERT_STATEMENT, values, 1000, 10000)
 
     return new_trips
