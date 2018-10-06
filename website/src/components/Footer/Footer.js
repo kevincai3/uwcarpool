e033d4cd8d8f6d64b319e63da5f7c60241c18d51
Kevin Cai <kevincai3@users.noreply.github.com>
2018-10-06 18:30:47 -0400

    Added hover states to buttons and links

--- website/src/components/Footer/Footer.js
+++ website/src/components/Footer/Footer.js
@@ -1,12 +1,3 @@
-/**
- * React Starter Kit (https://www.reactstarterkit.com/)
- *
- * Copyright © 2014-present Kriasoft, LLC. All rights reserved.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE.txt file in the root directory of this source tree.
- */
-
 import React from 'react';
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import classNames from 'classnames';
@@ -19,9 +10,9 @@ class Footer extends React.PureComponent {
       <div className={s.root}>
         <hr className={s.line} />
         <div className={s.container}>
-          <Link className={s.link} to="/about">FAQ</Link>
+          <Link className={classNames(s.link, s.highlight)} to="/about">FAQ</Link>
           <i className={classNames("fas fa-circle", s.dot)}/>
-          <a href="mailto:uwaterloocarpool@gmail.com">Email Us</a>
+          <a className={s.highlight} href="mailto:uwaterloocarpool@gmail.com">Email Us</a>
         </div>
       </div>
     );
